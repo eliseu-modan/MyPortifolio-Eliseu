a31f7d8d6476a68e7c141de722911a04ae8c2a06
Eliseumodanesi <68130532+Eliseumodanesi@users.noreply.github.com>
2022-01-31 01:18:41 -0300

    MyPortifolio,1,0
    Primeiro versionamento portifolio

---
+++ arquivo.js
@@ -0,0 +1,61 @@
+$(document).ready(function (){
+    
+
+//sistema de comentarios
+$('#comment').click(function() {
+
+var input = $('#input').val();
+var nome = $('#cnome').val();
+
+
+
+ 
+  if(nome != 0){
+   nome = nome + ':';
+$('.box').append(  nome +'<br>'   + input + '<br>' +'<br>' );
+$('#cnome').val('');
+$('#input').val('');
+
+
+$('.box-container').slideDown();
+
+
+  }
+});
+$('#cancel').click(function(){
+  $('#input').val('');
+  $('#cnome').val('');
+
+});
+
+$('#delete').click(function(){
+
+  
+   $('.box').text(' ');
+
+
+});
+
+$('#fechar').click(function(){
+$('.box-container').slideUp();
+
+
+})
+
+//dropdow cabeçalho Projetos
+$('.dropdowProjetos').mouseleave(function(e){
+e.preventDefault();
+$('.projetosd').css( 'cursor','pointer');
+$('.projetosd').css( 'display','block');
+
+
+});
+$('body').click(function(){
+$('.projetosd').css('display','none');
+})
+    
+
+
+});
+
+
